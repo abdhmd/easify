d4e9c0fffa6352f0d2a5244a0d990cd4134bfac0
abdhmd <sirhmd@outlook.com>
2024-04-05 14:42:56 +0100

    easify v 1.0.5

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -58,6 +58,7 @@ const Hero = ({ data }) => {
               </motion.button>
             </Link>
           </motion.div>
+          {/* just a comment  */}
         </div>
         <motion.div
           initial="hidden"
